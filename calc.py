3ddcb73cb99c3cb58a70de350696cb7e19db6472
assh.dev <asshihmin@yandex.ru>
2026-03-20 16:03:50 +0300

    123

--- calc.py
+++ calc.py
@@ -1,12 +1,37 @@
-# put your python code here
-var = input()
-try:
-    num = int(var)
-    if not 0 < num <= 3:
-        raise ValueError('Число вне диапазона')
-    print(var)
+while True:
+#умножение
+    def multiplication(num1, num2):
+        return num1 * num2
 
-except:
-    print('Некорректный ввод')
+# деление
+    def division(num1, num2):
+        return num1 / num2
 
+# сложение
+    def plus(num1, num2):
+        return num1 + num2
 
+    def minus(num1, num2):
+        return num1 - num2
+
+    try:
+        num1 = float(input('Введи первое  число:'))
+        znak = input('Введи знак операции:')
+        num2 = float(input('Введи второе число:'))
+
+        if znak == '*':
+            print(multiplication(num1, num2))
+        elif znak == '/':
+            print(division(num1, num2))
+        elif znak == '+':
+            print(plus(num1, num2))
+        elif znak == '-':
+            print(minus(num1, num2))
+        else:
+            print("Неверный знак операции")
+        print('Введи Q выхода')
+    except ZeroDivisionError:
+        print('Делить на ноль нельзя')
+
+    except ValueError:
+        print('Введены некорректные значения')
